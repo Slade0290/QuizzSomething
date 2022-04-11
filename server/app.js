930ca805c3fa729ba615a2d23561aad1fded5cd3
Slade0290 <ly.david@live.fr>
2022-04-11 12:48:18 +0200

    Show room list & player list. Test OK.

--- server/app.js
+++ server/app.js
@@ -1,21 +1,27 @@
 
 const express = require('express');
 
-
 const app = express();
-
-
 
 const server = app.listen(3001, function() {
     console.log('server running on port 3001');
 });
 
-
 const io = require('socket.io')(server);
+
+let rooms = [{message: "test room"}];
+let players = [{message: "test player"}];
 
 io.on('connection', function(socket) {
     console.log(socket.id)
-    socket.on('SEND_MESSAGE', function(data) {
-        io.emit('MESSAGE', data)
+
+    // LIST ROOMS
+    socket.on('GET:ROOMS', () => {
+        socket.emit('LIST:ROOMS', rooms)
+    });
+
+    // LIST PLAYERS
+    socket.on('GET:PLAYERS', () => {
+        socket.emit('LIST:PLAYERS', players)
     });
 });
--- src/components/WaitingRoom.vue
+++ src/components/WaitingRoom.vue
@@ -8,9 +8,19 @@
         </div>
         <div class="rooms-list">
             <h3>Liste des salons</h3>
+            <ul>
+                <li v-for="room in rooms" :key="room.message">
+                    {{ room.message }}
+                </li>
+            </ul>
         </div>
         <div class="players-list">
             <h3>Liste des joueurs</h3>
+            <ul>
+                <li v-for="player in players" :key="player.message">
+                    {{ player.message }}
+                </li>
+            </ul>
         </div>
     </section>
 </template>
@@ -29,17 +39,26 @@ export default {
                 socketId: '',
                 win: false
             },
-            socket : io('localhost:3001')
+            socket : io('localhost:3001'),
+            rooms: [],
+            players: []
         }
     },
     mounted() {
-        this.socket.emit('get rooms')
+        this.socket.emit('GET:ROOMS')
+        this.socket.on('LIST:ROOMS', (rooms) => {
+            this.rooms = rooms
+        })
+        this.socket.emit('GET:PLAYERS')
+        this.socket.on('LIST:PLAYERS', (players) => {
+            this.players = players
+        })
     }
 }
 </script>
 
 <style>
 .players-list {
-    display: none;
+    /*display: none;*/
 }
 </style>
