96ae86e57157de51ee969c1ff51c6c174ce9e9c2
Slade0290 <ly.david@live.fr>
2022-04-14 13:47:04 +0200

    Adding some design.

--- src/components/WaitingRoom.vue
+++ src/components/WaitingRoom.vue
@@ -1,19 +1,20 @@
 <template>
   <section>
-    <div class="create-room">
-      <div class="mb-3">
-        <label for="username" class="form-label">Nom d'utilisteur</label>
-        <input
-          type="text"
-          class="form-control"
-          ref="username"
-          id="username"
-          minlength="2"
-          maxlength="20"
-          placeholder="Saisir votre nom d'utilisateur"
-          required
-        />
-        <button v-on:click="submit">Créer un salon privée</button>
+    <h1 class="title">Olympie</h1>
+    <h2 class="subtitle">Mouseîon</h2>
+    <div class="container">
+      <div class="question-answer box">
+        <div class="box-inner">
+          <label for="username" class="question">What's your name ?</label>
+          <input
+            type="text"
+            ref="username"
+            id="username"
+            class="answer"
+            required
+          />
+          <button v-on:click="submit" class="validate-answer">Créer un salon privée</button>
+        </div>
       </div>
       <div v-if="player.host">Waiting...</div>
       <div v-else class="rooms-list">
@@ -101,3 +102,73 @@ export default {
   },
 };
 </script>
+
+<style>
+  @font-face {
+    font-family: 'Greconian';
+    src: url(../assets/fonts/Greconian.ttf);
+  }
+  html, body, #app{
+    height: 100%;
+    background: radial-gradient(#003, #000);
+    overflow: hidden;
+    color: white;
+  }
+  section {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    flex: 0 1;
+    height: 100%;
+  }
+  input {
+    border: none;
+  }
+  button {
+    border: none;
+  }
+  .title {
+    font-family: 'Greconian';
+    position: absolute;
+    padding: 1rem;
+    font-size: 2em;
+  }
+  .subtitle {
+    font-size: 2em;
+    padding: 1rem;
+  }
+  .question {
+    font-size: 4em;
+  }
+  .answer {
+    margin: 5rem;
+    font-size: 4em;
+    padding: 1.5rem;
+  }
+  .validate-answer {
+    margin: 4rem;
+    height: 2em;
+    font-size: 2em;
+    background: #4c956c;
+  }
+  .validate-answer:hover {
+    background: #2c6e49;
+  }
+  .question-answer {
+    display: flex;
+    flex-direction: column;
+  }
+  .box {
+    border: 2px solid #b78846;
+    padding: 5px;
+  }
+  .box-inner {
+    border: 2px solid #b78846;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+  }
+  .rooms-list {
+    padding: 1rem;
+  }
+</style>
