e6954fc9cec99eddf322560ece20fd8b3c47c88f
Slade0290 <ly.david@live.fr>
2022-04-21 13:48:31 +0200

    Update CSS Design & button.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+    <!--
     <div id="nav">
       <router-link to="/">Lobby</router-link> |
       <router-link to="/quiz">Quiz</router-link>
     </div>
+      -->
     <router-view/>
   </div>
 </template>
