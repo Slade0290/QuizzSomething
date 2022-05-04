4b64c5c543f787dd8c90caf8f55752715214a23d
Slade0290 <ly.david@live.fr>
2022-05-04 10:57:39 +0200

    Cleaning use less code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    <!--
-    <div id="nav">
-      <router-link to="/">Lobby</router-link> |
-      <router-link to="/quiz">Quiz</router-link>
-    </div>
-      -->
     <router-view/>
   </div>
 </template>
