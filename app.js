7134cbbfc6ba896f1893875ca51adac73c5be7a6
Slade0290 <ly.david@live.fr>
2022-05-12 12:32:39 +0200

    Working locally.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import socketIO from 'socket.io';
+import http from 'http'
 
 const app = express();
+let server = http.Server(app)
 const io = socketIO(server)
 
-app.listen(3001, function () {
-    console.log('server running on port 3001');
+server.listen(3000, function () {
+    console.log('server running on port 3000');
 });
 
 io.sockets.on('connection', function (socket) {
