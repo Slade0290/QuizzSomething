af24fa7a8c53d3f4cdf33767ca9cd3857346669c
Slade0290 <ly.david@live.fr>
2022-05-12 12:28:05 +0200

    Removing http.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import socketIO from 'socket.io';
-import http from 'http'
 
 const app = express();
-let server = http.Server(app)
 const io = socketIO(server)
 
-server.listen(3001, function () {
+app.listen(3001, function () {
     console.log('server running on port 3001');
 });
 
